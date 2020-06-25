66ab5fa5795e22d4af6011fbfd50095d8ab79f55
James Lefrère <james@colony.io>
2020-06-25 11:57:46 +0200

    Re-enable fee paid tx

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,12 +1,10 @@
-import { log, BigInt } from '@graphprotocol/graph-ts'
+import { BigInt } from '@graphprotocol/graph-ts'
 import { Basset, FeePaidTransaction, SwapTransaction } from './../../generated/schema'
 import { PaidFee, Swapped } from './../../generated/MUSD/Masset'
 import { RATIO, toDecimal } from '../utils/number'
-import { mapTransactionType, TransactionType } from '../enums'
 
 export function getOrCreateSwapTransaction(event: Swapped): SwapTransaction {
   let txHash = event.transaction.hash
-  // let type = mapTransactionType(TransactionType.SWAP)
   let type = 'SWAP'
   let id = txHash.toHexString().concat(type)
   let transaction = SwapTransaction.load(id)
@@ -31,16 +29,14 @@ export function getOrCreateSwapTransaction(event: Swapped): SwapTransaction {
   return transaction as SwapTransaction
 }
 
-export function getOrCreateFeePaidTransaction(event: PaidFee): void {
+export function getOrCreateFeePaidTransaction(event: PaidFee): FeePaidTransaction {
   let txHash = event.transaction.hash
-  // let type = mapTransactionType(TransactionType.PAIDFEE)
   let type = 'PAIDFEE'
   let id = txHash.toHexString().concat(type)
   let transaction = FeePaidTransaction.load(id)
 
   if (transaction != null) {
-    // return transaction as FeePaidTransaction
-    return
+    return transaction as FeePaidTransaction
   }
 
   transaction = new FeePaidTransaction(id)
@@ -48,15 +44,13 @@ export function getOrCreateFeePaidTransaction(event: PaidFee): void {
   transaction.type = type
   transaction.mAsset = event.address.toHex()
   let paidInBasset = Basset.load(event.params.asset.toHexString())
-  log.info("feeQuantity {}", [event.params.feeQuantity.toString()])
-  return
-  // let ratioedOutputAmount = BigInt.fromI32(<i32>event.params.feeQuantity as i32)
-  //   .times(paidInBasset.ratio)
-  //   .div(RATIO)
-  // transaction.mAssetUnits = toDecimal(ratioedOutputAmount, 18)
-  // transaction.timestamp = event.block.timestamp.toI32()
-  // transaction.sender = event.params.payer
-  // transaction.asset = event.params.asset.toHexString()
-  // transaction.save()
-  // return transaction as FeePaidTransaction
+  let ratioedOutputAmount = BigInt.fromI32(<i32>event.params.feeQuantity as i32)
+    .times(paidInBasset.ratio)
+    .div(RATIO)
+  transaction.mAssetUnits = toDecimal(ratioedOutputAmount, 18)
+  transaction.timestamp = event.block.timestamp.toI32()
+  transaction.sender = event.params.payer
+  transaction.asset = event.params.asset.toHexString()
+  transaction.save()
+  return transaction as FeePaidTransaction
 }
